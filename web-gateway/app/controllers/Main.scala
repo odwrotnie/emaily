15ca95ad6cd09aed275cafc9ef10d6022d166b05
rzepaw <odwrotnie@gmail.com>
2018-02-25 14:51:28 +0100

    hello

--- web-gateway/app/controllers/Main.scala
+++ web-gateway/app/controllers/Main.scala
@@ -2,12 +2,14 @@ package controllers
 
 import javax.inject._
 
+import it.wext.emaily.api.EmailyService
 import play.api.mvc._
 
 @Singleton
-class Main @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class Main @Inject()(emailyService: EmailyService,
+                     cc: ControllerComponents) extends AbstractController(cc) {
 
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+    Ok(views.html.index("asdf"))
   }
 }
